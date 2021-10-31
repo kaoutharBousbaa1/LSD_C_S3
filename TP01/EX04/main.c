270ab896caf1e5af0984f919f39b9aec143b9011
Kaouthar BOUSBAA <80353549+kaoutharBousbaa1@users.noreply.github.com>
2021-11-01 00:52:20 +0100

    Update main.c

--- TP01/EX04/main.c
+++ TP01/EX04/main.c
@@ -2,8 +2,7 @@
 #include <stdlib.h>
 #include 'print_nth_bit.h'
 
-int main(int argc, int *argv[])
-{
+int main(int argc, char *argv[]){
     print_nth_byte(1, 190);
     print_nth_byte(2, 190);
     print_nth_byte(3, 190);
