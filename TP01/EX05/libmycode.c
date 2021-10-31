d2705862f7b8b53a0c23c0a80a2fb1db0e055c0c
Kaouthar BOUSBAA <80353549+kaoutharBousbaa1@users.noreply.github.com>
2021-11-01 00:54:28 +0100

    Update libmycode.c

--- TP01/EX05/libmycode.c
+++ TP01/EX05/libmycode.c
@@ -1,47 +1,39 @@
 #include <stdio.h>
 #include 'mycode.h'
 
-void print_bits(int number)
-{
+void print_bits(int number){
     int i;
     i = 31;
-    while(i >= 1)
-    {
+    while(i >= 1){
         (number & (1 << i)) ? printf("1 ") : printf("0 ");
         i--;
     }
     (number & (1 << i)) ? printf("1 ") : printf("0 ");
     printf("\n");
 }
-void check_msb(int num)
-{
+void check_msb(int num){
     (num >> 31) ? printf("The most significant bit is set.\n") : printf("The most significant bit is not set");
 }
-void print_nth_byte(int n, int m)
-{
+void print_nth_byte(int n, int m){
         int j = 0;
-        if(n == 4)
-        {
+        if(n == 4){
             for(j = 31; j >= 24; j--)
                 (m & (1 << j)) ? printf("1 ") : printf("0 ");
             printf("\n");
         }
-        if(n == 1)
-        {
+        if(n == 1){
             int l = m << 24 ;
             for(j = 31; j >= 24; j--)
                 (l & (1 << j)) ? printf("1 ") : printf("0 ");
             printf("\n");
         }
-        if(n == 2)
-        {
+        if(n == 2){
             int l = m << 16 ;
             for(j = 31; j >= 24; j--)
                 (l & (1 << j)) ? printf("1 ") : printf("0 ");
             printf("\n");
         }
-        if(n == 3)
-        {
+        if(n == 3){
             int l = m << 8 ;
             for(j = 31; j >= 24; j--)
                 (l & (1 << j)) ? printf("1 ") : printf("0 ");
