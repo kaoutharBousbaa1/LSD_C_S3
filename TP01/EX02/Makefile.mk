e53b1c29931e49aa04d0f984178d3f25973597a9
Kaouthar BOUSBAA <80353549+kaoutharBousbaa1@users.noreply.github.com>
2021-10-31 21:01:59 +0100

    Update Makefile.mk

--- TP01/EX02/Makefile.mk
+++ TP01/EX02/Makefile.mk
@@ -3,10 +3,10 @@
 
   all: Bitwise
   Bitwise: main.o Bitwise.o
-			$(CC) main.o Bitwise.o -o Bitwise
+		$(CC) main.o Bitwise.o -o Bitwise
   main.o: main.c
-		   $(CC) $(CFLAGS) main.c
+		$(CC) $(CFLAGS) main.c
   Bitwise.o: Bitwise.c
-			$(CC) $(CFLAGS) Bitwise.c
+		$(CC) $(CFLAGS) Bitwise.c
 
   clean: rw -rf *o Bitwise 
