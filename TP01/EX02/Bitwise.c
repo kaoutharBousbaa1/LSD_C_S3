36e28fec90b1a2e4f2a3c1fcd50c89206173b66f
Kaouthar BOUSBAA <80353549+kaoutharBousbaa1@users.noreply.github.com>
2021-10-31 20:39:54 +0100

    Update Bitwise.c

--- TP01/EX02/Bitwise.c
+++ TP01/EX02/Bitwise.c
@@ -14,38 +14,6 @@ void print_bits(int number)
     (number & (1 << i)) ? printf("1 ") : printf("0 ");
     printf("\n");
 }
-void print_nth_byte(int n, int m)
-{
-        int j = 0;
-        if(n == 4)
-        {
-            for(j = 31; j >= 24; j--)
-                (m & (1 << j)) ? printf("1 ") : printf("0 ");
-            printf("\n");
-        }
-        if(n == 1)
-        {
-            int l = m << 24 ;
-            for(j = 31; j >= 24; j--)
-                (l & (1 << j)) ? printf("1 ") : printf("0 ");
-            printf("\n");
-        }
-        if(n == 2)
-        {
-            int l = m << 16 ;
-            for(j = 31; j >= 24; j--)
-                (l & (1 << j)) ? printf("1 ") : printf("0 ");
-            printf("\n");
-        }
-        if(n == 3)
-        {
-            int l = m << 8 ;
-            for(j = 31; j >= 24; j--)
-                (l & (1 << j)) ? printf("1 ") : printf("0 ");
-            printf("\n");
-        }
-
-}
 /*unsigned char decimalToBinary(unsigned char decimalnumber)
 {
     unsigned char binarynumber = 0;
