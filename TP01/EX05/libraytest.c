2a0dd2e1ff982563b8cf9277677338b79eee5d7d
Kaouthar BOUSBAA <80353549+kaoutharBousbaa1@users.noreply.github.com>
2021-11-01 00:55:28 +0100

    Update libraytest.c

--- TP01/EX05/libraytest.c
+++ TP01/EX05/libraytest.c
@@ -2,9 +2,9 @@
 #include 'mycode.h'
 
 
-int main(int argc, int *argv[]){
+int main(int argc, char *argv[]){
     print_nth_byte(1, 190);
     check_msb(2);
     print_bits(45);
     return 0; 
-})
+}
