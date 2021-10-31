e8c18f7702bc1b50dbbc70427d9250d7649d8530
Kaouthar BOUSBAA <80353549+kaoutharBousbaa1@users.noreply.github.com>
2021-11-01 00:53:08 +0100

    Update print_nth_bit.c

--- TP01/EX04/print_nth_bit.c
+++ TP01/EX04/print_nth_bit.c
@@ -2,31 +2,26 @@
 #include <stdlib.h>
 #include 'print_nth_bit.h'
 
-void print_nth_byte(int n, int m)
-{
+void print_nth_byte(int n, int m){
         int j = 0;
-        if(n == 4)
-        {
+        if(n == 4){
             for(j = 31; j >= 24; j--)
                 (m & (1 << j)) ? printf("1 ") : printf("0 ");
             printf("\n");
         }
-        if(n == 1)
-        {
+        if(n == 1){
             int l = m << 24 ;
             for(j = 31; j >= 24; j--)
                 (l & (1 << j)) ? printf("1 ") : printf("0 ");
             printf("\n");
         }
-        if(n == 2)
-        {
+        if(n == 2){
             int l = m << 16 ;
             for(j = 31; j >= 24; j--)
                 (l & (1 << j)) ? printf("1 ") : printf("0 ");
             printf("\n");
         }
-        if(n == 3)
-        {
+        if(n == 3){
             int l = m << 8 ;
             for(j = 31; j >= 24; j--)
                 (l & (1 << j)) ? printf("1 ") : printf("0 ");
