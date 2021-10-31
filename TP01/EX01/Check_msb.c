71f684008e57c44e2fec5cac067c8d1d9b97a4e1
Kaouthar BOUSBAA <80353549+kaoutharBousbaa1@users.noreply.github.com>
2021-11-01 00:49:37 +0100

    Update Check_msb.c

--- TP01/EX01/Check_msb.c
+++ TP01/EX01/Check_msb.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void check_msb(int num)
-{
+void check_msb(int num) {
     (num >> 31) ? printf("The most significant bit is set.\n") : printf("The most significant bit is not set");
 }
-int main (int argc, char *argv[])
-{
+int main (int argc, char *argv[]){
     check_msb(2);
+    return 0;
 }
