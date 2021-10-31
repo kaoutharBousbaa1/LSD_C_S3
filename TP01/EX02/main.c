bf18c28093c0005556cc3bfcdaf8c04357c7ebed
Kaouthar BOUSBAA <80353549+kaoutharBousbaa1@users.noreply.github.com>
2021-10-31 20:48:21 +0100

    Update main.c

--- TP01/EX02/main.c
+++ TP01/EX02/main.c
@@ -5,9 +5,6 @@
 int main(int argc, char *argv[])
 {
     print_bits(190);
-    print_nth_byte(1, 190);
-    print_nth_byte(2, 190);
-    print_nth_byte(3, 190);
-    print_nth_byte(4, 190);
+    check_msb(2);
 }
 
