97fe1af3954aa15cf0a4c0493d3fe375f754a286
Kaouthar BOUSBAA <80353549+kaoutharBousbaa1@users.noreply.github.com>
2021-11-05 23:46:42 +0100

    Add files via upload

---
+++ TP01/EX07/libraytest.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "mycode.h"
+
+
+int main(int argc, int *argv[])
+{
+    print_nth_byte(1, 190);
+    check_msb(2);
+    print_bits(45);
+    return 0; 
+}
---
+++ TP01/EX07/mycode.h
@@ -0,0 +1,8 @@
+# ifndef mycode
+# define mycode
+
+void print_bits(int number);
+void check_msb(int num);
+void print_nth_byte(int n, int m);
+
+# endif
