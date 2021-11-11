30604c494460ef7111350034623fef264972f84b
Kaouthar BOUSBAA <80353549+kaoutharBousbaa1@users.noreply.github.com>
2021-11-11 16:55:04 +0100

    Update readline.c

--- TP02/EX07/readline.c
+++ TP02/EX07/readline.c
@@ -5,25 +5,6 @@
 #include <sys/stat.h> 
 #include <unistd.h>
 
-/*char* get_next_line(int fd)
-{
-	char* line;
-	int n_bytes;
-	void* BUFFER_SIZE[20];
-	if(n_bytes = read(fd, BUFFER_SIZE, 10))
-	{
-		if (line == NULL) 
-            	{   
-              		printf("Error! Could not open file\n"); )
-			return NULL;
-            	} 
-		while(*line == ' ' || *line =='\t')
-			line++;
-		return line;
-	}
-	else
-		return NULL;
-*/
 char* get_next_line(int fd)
 {
 	char* cr = malloc(sizeof(char)*50);
@@ -32,14 +13,6 @@ char* get_next_line(int fd)
 }
 int main(int argc, int* argv[])
 {	
-	/*FILE* filename = "file.txt";
-	FILE* file = fopen(filename, "r");
-	int fd = open("file.txt", O_RDONLY);
-	char* line;
-	while(line != "\0")
-		line = get_next_line(fd);
-	fclose(file);
-	return 0;*/
 	int fd = open("newfile.txt", O_RDONLY);
 	char* rt = get_next_line(fd);
 	printf("Test %s", rt);
