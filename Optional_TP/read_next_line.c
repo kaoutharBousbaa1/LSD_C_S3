498c59eefff8e68844f80a624b49b8f83fbf4eaf
Kaouthar BOUSBAA <80353549+kaoutharBousbaa1@users.noreply.github.com>
2021-12-10 20:27:32 +0100

    Update read_next_line.c

--- Optional_TP/read_next_line.c
+++ Optional_TP/read_next_line.c
@@ -5,26 +5,93 @@
 #include <sys/stat.h> 
 #include <unistd.h>
 
-
+int x = BUFSIZ;
+int st_len(char *s)
+{
+	char* temp;
+	int i = 0;
+	if(s == NULL)
+		return 0;
+	temp = s;
+	while(temp[i] != '\0')
+	{
+		temp = temp + i;
+		i++;
+	}
+	return (i+1);
+}
+char 	*ch_join(char *s1, char *s2)
+{
+	char* temp;
+	int i, j;
+	if(!s1 || !s2)
+		return NULL;
+	temp = malloc(((st_len(s1) + st_len(s2) + 1) * sizeof(char)));
+	if(!temp)
+		return NULL;
+	i = 0;
+	j = 0;
+	while(i < st_len(s1))
+	{
+		temp[i] = s1[i];
+		i++;
+	}
+	while(j < st_len(s2))
+	{
+		temp[j + i] = s2[j];
+		j++;
+	}
+	return temp;
+}
+char* get_line(char *c)
+{
+	char* line;
+	int i;
+	char* tab;
+	if(c = NULL)
+		return NULL;
+	i = 0;
+	while(c[i] != '\n')
+		i++;
+	i++;
+	tab = (char*)malloc(sizeof(char) * (i+1));
+	tab[i] = '\0';
+	return tab;
+}
+char 		*get_next_line(int fd)
+{
+	char 			buff[BUFSIZ];
+	char			*line;
+	static char		*c;
+	int 			byte;
+	if(BUFSIZ <= 0 || fd < 0)
+		return NULL;
+	byte = 1;
+	while(byte > 0)
+	{
+		byte = read(fd, buff, BUFSIZ);
+		if(byte == -1)
+			return NULL;
+		else if(byte == 0)
+		{
+			buff[byte] = '\0';
+			break;
+		}
+		c = ch_join(c, buff);	
+	}
+	line = get_line(c);
+	return line;
+}
 int main(int argc, int* argv[])
 {	
-  fd1 = open(av[1], O_RDONLY);
-  fd2 = open(av[2], O_RDONLY);
-  get_next_line(fd1, &line);
-  printf("%s\n", line);
-  get_next_line(fd1, &line);
-  printf("%s\n", line);
-  get_next_line(fd2, &line);
-  printf("%s\n", line);
-  get_next_line(fd2, &line);
-  printf("%s\n", line);
-  get_next_line(fd1, &line);
-  printf("%s\n", line);
-  get_next_line(fd2, &line);
-  printf("%s\n", line);
-  get_next_line(fd1, &line);
-  printf("%s\n", line);
-  get_next_line(fd2, &line);
-  printf("%s\n", line);
-  return (0);
+	int fd = open("newfile.txt", O_RDONLY);
+	char* line = NULL;	
+	line = get_next_line(fd);
+	while(line != NULL)
+	{
+		line = get_next_line(fd);
+		printf("%s", line);
+	}
+	return 0;
 }
+
