a0f02b92caaab33e6451d2163f5a7e7dc7be6a64
Kaouthar BOUSBAA <80353549+kaoutharBousbaa1@users.noreply.github.com>
2021-12-16 12:51:29 +0100

    Update read_next_line.c

--- Optional_TP/read_next_line.c
+++ Optional_TP/read_next_line.c
@@ -18,7 +18,7 @@ int st_len(char *s)
 }
 char 	*ch_join(char *s1, char *s2)
 {
-	char* temp;
+	char* temp = NULL;
 	int i, j;
 	if(!s1 || !s2)
 	{
@@ -41,38 +41,48 @@ char 	*ch_join(char *s1, char *s2)
 	temp[j+i] = '\0';
 	return temp;
 }
-void *stcpy(void *s1, void *s2, int n)
+void *stcpy(void *s1, void *s2, int n, int k)
 {
 	char	*temp1;
 	char	*temp2;
 	temp1 = (char*)s1;
 	temp2 = (char*)s2;
-	while(n--)
-		temp1[n] = temp2[n];
+	while(k < n)
+	{
+		temp1[k] = temp2[k];
+		k++;
+	}
 	return temp1;
 }
 char* get_line(char *c)
 {
 	char* line;
-	static int i = 0;
+	static int i;
+	int j = 0;
 	char* tab;
-	int j;
+	j = i;
 	if(c == NULL)
 		return NULL;
-	j = i;
 	while(c[i] != '\n')
 		i++;
 	i++;
 	tab = (char*)malloc(sizeof(char) * ((i+1)-j));
-	tab = stcpy(tab, c, i-j);
+	int k = 0;
+	while(k < i-j)
+	{
+		tab[k] = c[j+k];
+		k++;
+	}
+	//tab = stcpy(tab, c, i, j-i);
 	tab[i-j] = '\0';
+	//printf("%s", tab);
 	return tab;
 }
 char 		*get_next_line(int fd)
 {
 	char 			buff[BUFSIZ];
 	char			*line;
-	static char		*c;
+	char		*c;
 	int 			byte;
 	if(BUFSIZ <= 0 || fd < 0)
 		return NULL;
@@ -92,6 +102,7 @@ char 		*get_next_line(int fd)
 		else
 			c = ch_join(c, buff);	
 	}
+	//printf("%s", c);
 	line = get_line(c);
 	return line;
 }
@@ -99,6 +110,12 @@ int main(int argc, int* argv[])
 {	
 	int fd = open("newfile.txt", O_RDONLY);
 	char* line;
+	line = get_next_line(fd);
+	printf("%s", line);
+	line = get_next_line(fd);
+	printf("%s", line);
+	line = get_next_line(fd);
+	printf("%s", line);
 	line = get_next_line(fd);
 	printf("%s", line);
 	/*do
